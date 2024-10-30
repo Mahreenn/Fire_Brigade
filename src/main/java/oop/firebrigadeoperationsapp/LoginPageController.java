f3da7c335b3f08a20def34838afa0de556b91743
Mahreenn <mahreenarasin@gmail.com>
2024-10-30 17:44:24 +0600

    added EmployeeParent class and firefighter and Dispatcher model classes

--- src/main/java/oop/firebrigadeoperationsapp/LoginPageController.java
+++ src/main/java/oop/firebrigadeoperationsapp/LoginPageController.java
@@ -1,4 +1,25 @@
 package oop.firebrigadeoperationsapp;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class LoginPageController {
+
+    @FXML
+    private Button loginbutton;
+
+    @FXML
+    private Button signupbutton;
+
+    @FXML
+    void OnSignUpButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLoginButtonClick(ActionEvent event) {
+
+    }
+
 }
