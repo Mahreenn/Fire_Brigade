b79a9e573e1537e570e4f21590f2a2d8345112bd
abdus-sadik <159509599+abdus-sadik@users.noreply.github.com>
2024-11-10 18:43:18 +0600

    LoginPageController OnLoginButtonClick mod.

--- src/main/java/oop/firebrigadeoperationsapp/LoginPageController.java
+++ src/main/java/oop/firebrigadeoperationsapp/LoginPageController.java
@@ -3,7 +3,11 @@ package oop.firebrigadeoperationsapp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+
+
 
 public class LoginPageController {
 
@@ -12,22 +16,31 @@ public class LoginPageController {
 
     @FXML
     private Label messageLabel;
+    @FXML
+    private PasswordField passwordfield;
+
+    String username, password;
+
 
     @FXML
-    private TextField passwordTextfield;
-
-
-    @FXML
-    void OnSignUpButtonClick(ActionEvent event) {
-        String username = UsernameTextfield.getText();
-        if( username.isBlank()){
-            messageLabel.setText("Please enter username");
-        }
-
+    void OnSignUpButtonClick(ActionEvent event){
     }
 
     @FXML
     void onLoginButtonClick(ActionEvent event) {
+
+        if( !username.isEmpty()){
+            username= UsernameTextfield.getText();
+
+        }else {
+            messageLabel.setText("Please enter username");
+        }
+        if( !password.isEmpty()){
+            password= passwordfield.getText();
+
+        }else {
+            messageLabel.setText("Please enter password");
+        }
 
     }
 
