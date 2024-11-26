69f67ba408eff8381e3bfd5d1a880cada2149610
Mahreenn <mahreenarasin@gmail.com>
2024-11-26 20:05:38 +0600

    -alertModelclass, dispatcher controller

--- src/main/java/oop/firebrigadeoperationsapp/Alert.java
+++ src/main/java/oop/firebrigadeoperationsapp/Alert.java
@@ -11,4 +11,60 @@ public class Alert {
     private Boolean ambulanceReq;
     private Location location;
 
+    public Alert(LocalDate dateofAlert, LocalTime timeofAlert, String smalldescription, String areaType, Boolean ambulanceReq, Location location) {
+        this.dateofAlert = dateofAlert;
+        this.timeofAlert = timeofAlert;
+        this.smalldescription = smalldescription;
+        this.areaType = areaType;
+        this.ambulanceReq = ambulanceReq;
+        this.location = location;
+    }
+
+    public LocalDate getDateofAlert() {
+        return dateofAlert;
+    }
+
+    public void setDateofAlert(LocalDate dateofAlert) {
+        this.dateofAlert = dateofAlert;
+    }
+
+    public LocalTime getTimeofAlert() {
+        return timeofAlert;
+    }
+
+    public void setTimeofAlert(LocalTime timeofAlert) {
+        this.timeofAlert = timeofAlert;
+    }
+
+    public String getSmalldescription() {
+        return smalldescription;
+    }
+
+    public void setSmalldescription(String smalldescription) {
+        this.smalldescription = smalldescription;
+    }
+
+    public String getAreaType() {
+        return areaType;
+    }
+
+    public void setAreaType(String areaType) {
+        this.areaType = areaType;
+    }
+
+    public Boolean getAmbulanceReq() {
+        return ambulanceReq;
+    }
+
+    public void setAmbulanceReq(Boolean ambulanceReq) {
+        this.ambulanceReq = ambulanceReq;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
