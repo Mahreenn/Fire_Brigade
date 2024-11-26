474c016c5ccf0e2cff051acf6bbe9dd8e0ddc92c
Mahreenn <mahreenarasin@gmail.com>
2024-11-26 20:06:13 +0600

    external user fxml

--- src/main/java/oop/firebrigadeoperationsapp/Dispatcher/DispatcherDashboardController.java
+++ src/main/java/oop/firebrigadeoperationsapp/Dispatcher/DispatcherDashboardController.java
@@ -1,4 +1,57 @@
 package oop.firebrigadeoperationsapp.Dispatcher;
 
+import javafx.event.ActionEvent;
+
 public class DispatcherDashboardController {
+
+    @FXML
+    void onAssignTeamButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBackButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCheckSchedulebuttonclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onContactInventorySupplierbuttonclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onGenerateIncidentReportButtonclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogoutButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMakeTrainingschedulebuttonclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRealTimeLocationbuttonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRecieveeemergencyButtonclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewAvailableInventorybuttonclick(ActionEvent event) {
+
+    }
+
 }
